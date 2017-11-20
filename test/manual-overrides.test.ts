51a2b36f36ba5429e5070e16f40ad75b0792086b
mmkal <mmkal@users.noreply.github.com>
2017-11-19 19:58:57 -0500

    fix: simplifier tests

--- test/manual-overrides.test.ts
+++ test/manual-overrides.test.ts
@@ -3,5 +3,5 @@ import { _isFinite, _typeOf, _sorted, _firstTwoCharacters } from "./_manual-over
 
 test("isFinite", t => t.is(_isFinite(123), "isFinite => true"));
 test("typeof", t => t.is(_typeOf("foo"), "typeOf => string"));
-test("sorted", t => t.is(_sorted([3, 2, 1]), "sorted => [1, 2, 3]"));
-test("firstTwoCharacters", t => t.is(_firstTwoCharacters("hello"), "he***"));
+test("sorted", t => t.is(_sorted([3, 2, 1]), "sorted => [1,2,3]"));
+test("firstTwoCharacters", t => t.is(_firstTwoCharacters("hello"), "firstTwoCharacters => he***"));

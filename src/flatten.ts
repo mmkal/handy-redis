779e17aff609c51c708a08e21b6d5dbddf763ffc
mmkal <misha.kaletsky@gmail.com>
2019-12-24 02:11:46 +0000

    fix(flatten): use for loop to avoid stack issues

--- src/flatten.ts
+++ src/flatten.ts
@@ -1,6 +1,7 @@
-export const flattenDeep = (args: any[]): any[]  => {
-    if (!Array.isArray(args)) {
-        return args;
+export const flattenDeep = (array: any[]): any[]  => {
+    const flat = [] as any[];
+    for (const item of array) {
+        Array.isArray(item) ? flat.push(...flattenDeep(item)) : flat.push(item);
     }
-    return [].concat(...args.map(flattenDeep) as any);
+    return flat;
 };

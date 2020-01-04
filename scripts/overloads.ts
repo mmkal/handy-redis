f40c98ec7912ea9d57e3af8eed5f6332202c1620
mmkal <misha.kaletsky@gmail.com>
2020-01-04 02:06:19 +0000

    chore: simplify overloads while I'm here

--- scripts/overloads.ts
+++ scripts/overloads.ts
@@ -4,26 +4,16 @@ import { get } from "lodash";
 
 export const getOverloads = (args: Argument[]): Argument[][] => {
     if (args.length <= 1) {
-        const overloads = [args];
-        if (get(args, "[0].optional")) {
-            overloads.push([]);
-        }
-        return overloads;
+        return get(args, "[0].optional") ? [args, []] : [args];
     }
-    const first = args[0];
-    const theRest = args.slice(1);
-    let variations = new Array<Argument[]>();
-    const overloadsOfTheRest = getOverloads(theRest);
-    const argListsWithFirst = overloadsOfTheRest.map(argsSubList => [
-        first,
-        ...argsSubList
-    ]);
-    variations = variations.concat(argListsWithFirst);
-    if (first.optional) {
-        variations = variations.concat(overloadsOfTheRest);
+    const head = args[0];
+    const tailOverloads = getOverloads(args.slice(1));
+    const overloads = tailOverloads.map(argsSubList => [head, ...argsSubList]);
+    if (head.optional) {
+        overloads.push(...tailOverloads);
     }
 
-    const variationsWithMultipleTuplesFlattened = variations
+    const overloadsWithFlattenedTailTuples = overloads
         .filter(v => {
             const last = v[v.length - 1];
             return last && last.multiple && Array.isArray(last.type);
@@ -34,7 +24,7 @@ export const getOverloads = (args: Argument[]): Argument[][] => {
             return v.slice(0, -1).concat(types.map((t, i) => ({ ...last, name: last.name.split("_")[i], type: t, multiple: false })));
         });
 
-    variations = variationsWithMultipleTuplesFlattened.concat(variations);
+    overloads.unshift(...overloadsWithFlattenedTailTuples);
 
-    return variations;
+    return overloads;
 };

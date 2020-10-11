175bd1d7a3ddf5fc93e14ee6c2f787d9c31fe04d
mmkal <misha.kaletsky@gmail.com>
2020-10-11 10:59:48 -0400

    remove unused utils

--- scripts/json-schema/util.ts
+++ scripts/json-schema/util.ts
@@ -1,83 +1,17 @@
 /* istanbul ignore file */
-import { stringify as yamlify } from "yamljs";
-import { Command } from "./command";
-import { EOL } from "os";
-import { writeFileSync, existsSync, readdirSync } from "fs";
-import { dirname } from "path";
-import * as shelljs from "shelljs";
+import * as fs from "fs";
+import * as path from "path";
 import * as prettier from "prettier";
 
-export const tab = `    `;
-export const twotabs = `${tab}${tab}`;
-
-export const redisDoc = `scripts/redis-doc`;
-export const commandDoc = `${redisDoc}/commands`;
-
-export const indent = (input: string) =>
-    input
-        .split(/\r?\n/)
-        .join(EOL)
-        .split(EOL)
-        .map(line => `${tab}${line}`)
-        .join(EOL);
-
-export const quote = (input: string) => {
-    if (!input) {
-        return `""`;
-    }
-    if (input.indexOf(`"`) === -1) {
-        return `"${input}"`;
-    }
-    if (input.indexOf("`") === -1) {
-        return `\`${input}\``;
-    }
-    return JSON.stringify(input);
-};
-
-export const makeArrayType = (type: string) => (type.match(/\W/) ? `Array<${type}>` : `${type}[]`);
-
-export const simplifyName = (name: string) => name.toLowerCase().replace(/(?:^([0-9])|[^a-zA-Z0-9_$])/g, "_$1");
-
-export const getDocs = (command: Command) => yamlify(command).trim().replace(/\r?\n/g, `${EOL}     *${EOL}     * `); // docs render better when they're double-spaced
-
-export const run = async (task: () => void | Promise<any>) => {
-    try {
-        await task();
-        process.exit(0);
-    } catch (e) {
-        console.error(e); // tslint:disable-line no-console
-        process.exit(1);
-    }
-};
-
-export const buildScript = async (module: NodeModule, task: () => void | Promise<any>) => {
-    if (require.main === module) {
-        await run(task);
-    }
-};
-
-export const writeFile = (filename: string, contents: string) => {
-    const filedir = dirname(filename);
-    if (!existsSync(filedir)) {
-        shelljs.mkdir("-p", filedir);
-    }
-    if (!contents.endsWith("\n")) {
-        contents += EOL;
-    }
+export const writeFile = (filepath: string, contents: string) => {
+    fs.mkdirSync(path.dirname(filepath), { recursive: true });
     try {
         contents = prettier.format(contents, {
             ...require("../../.prettierrc"),
-            filepath: filename,
+            filepath,
         });
     } catch (e) {
-        console.warn(`prettier failed for ${filename}: ${e}`.slice(0, 200));
+        console.warn(`prettier failed for ${filepath}: ${e}`.slice(0, 200));
     }
-    writeFileSync(filename, contents, "utf8");
-};
-
-export const readdirWithPaths = (dir: string) => {
-    if (!existsSync(dir)) {
-        return [];
-    }
-    return readdirSync(dir).map(f => `${dir}/${f}`);
+    fs.writeFileSync(filepath, contents, "utf8");
 };

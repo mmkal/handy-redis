c28c4b1ef9944677b240632c35d06a56f700a40a
mmkal <misha.kaletsky@gmail.com>
2020-11-25 11:16:37 -0500

    Add postbuild comment

--- src/type-util.ts
+++ src/type-util.ts
@@ -1,5 +1,6 @@
-// Variadic tuple prefixes only work in ts>4. To support lower typescript versions, check if the feature works
-// this means we need ts-ignore, not ts-expect-error because it's _not_ an error in ts>4
+// Rest elements must be last in tuple types prior to typescript 4.x. To support lower typescript versions, check if the feature works
+// this means we need ts-ignore, not ts-expect-error because it's _not_ an error in ts>=4
+// Note: this ts-ignore doesn't get emitted to d.ts, so it needs to be re-added in "postbuild" (see package.json)
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 type Push_ts4<A extends unknown[], B> = [...A, B];
